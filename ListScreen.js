fbcece1625a0ffdec38e79332acb923de6789fad
Obspogon <72287594+Obspogon@users.noreply.github.com>
2025-06-22 19:52:21 -0400

    implement deleting transactions

--- ListScreen.js
+++ ListScreen.js
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Pressable, FlatList } from "react-native";
 
 const ListScreen = ({ navigation, route }) => {
-	let transactions = [
+	const [transactions, setTransactions] = useState([
 		{ id: 1, name: "Burger", amount: 9.99, location: "Burger King", date: "15-3-25", expense: true, category: "Food" },
 		{ id: 2, name: "PRESTO", amount: 15, location: "PRESTO Terminal", date: "11-4-25", expense: true, category: "Transportation" },
 		{ id: 3, name: "Paycheck", amount: 150, location: "Online", date: "12-4-25", expense: false, category: "Salary" },
 		{ id: 4, name: "Gift", amount: 50, location: "Online", date: "15-4-25", expense: false, category: "Other" },
 		{ id: 5, name: "Chocolate", amount: 5.99, location: "Store", date: "16-4-25", expense: true, category: "Food" },
-	];
+	]);
+
 	let total = 0;
 	transactions.forEach((item) => {
 		total = item.expense ? total - item.amount : total + item.amount;
@@ -38,9 +40,11 @@ const ListScreen = ({ navigation, route }) => {
 									style={({ pressed }) => [styles.button, { backgroundColor: pressed ? "red" : "orangered" }]}
 									onPress={() => {
 										// alert(`Deleted. id: ${item.id}`);
-										transactions = transactions.filter((t) => {
-											return t.id !== item.id;
-										});
+										setTransactions(
+											transactions.filter((t) => {
+												return t.id !== item.id;
+											})
+										);
 										navigation.navigate("ListScreen");
 									}}
 								>
