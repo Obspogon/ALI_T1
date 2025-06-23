31d98554512a92ee6f139bdacef06277782986c3
Obspogon <72287594+Obspogon@users.noreply.github.com>
2025-06-22 21:19:12 -0400

    move transactions to external file

--- ListScreen.js
+++ ListScreen.js
@@ -1,14 +1,9 @@
 import { useState } from "react";
 import { StyleSheet, Text, View, Pressable, FlatList } from "react-native";
+import Transactions from "./Transactions";
 
 const ListScreen = ({ navigation, route }) => {
-	const [transactions, setTransactions] = useState([
-		{ id: 1, name: "Burger", amount: 9.99, location: "Burger King", date: "15-3-25", expense: true, category: "Food" },
-		{ id: 2, name: "PRESTO", amount: 15, location: "PRESTO Terminal", date: "11-4-25", expense: true, category: "Transportation" },
-		{ id: 3, name: "Paycheck", amount: 150, location: "Online", date: "12-4-25", expense: false, category: "Salary" },
-		{ id: 4, name: "Gift", amount: 50, location: "Online", date: "15-4-25", expense: false, category: "Other" },
-		{ id: 5, name: "Chocolate", amount: 5.99, location: "Store", date: "16-4-25", expense: true, category: "Food" },
-	]);
+	const [transactions, setTransactions] = useState(Transactions);
 
 	let total = 0;
 	transactions.forEach((item) => {
